b36e26f09e8c2c2728033b0ef50e79d230c67785
SCWM- <SCWM-P@outlook.com>
2024-02-27 16:08:09 +0800

    Initial commit

--- SelectArea.py
+++ SelectArea.py
@@ -5,45 +5,32 @@ from dv import AedatFile
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-with AedatFile(r'data/test_2mm_4Hz.aedat4') as f:
-    height, width = f['events'].size
-    events = np.hstack([packet for packet in f['events'].numpy()])
-    T, xEvent, yEvent, polarities = events['timestamp'], events['x'], events['y'], events['polarity']
-    T = (T - T[0])/1e6
-
 
 def HotPixel_brush(xEvent, T, yEvent):
     """
-    :param xEvent:
-    :param T:
-    :param yEvent:
-    :return: Brushed xEvent, T, yEvent
-    This function is used to brush the data via 3-sigma rule
+    :param xEvent: Event data of x
+    :param T: Event data of time
+    :param yEvent: Event data of y
+    :return: filtered event data
     """
-    df = pd.DataFrame({'T': T, 'yEvent': yEvent, 'xEvent': xEvent})
-    df['coords'] = list(zip(df.xEvent, df.yEvent))
-    unique_coords = df['coords'].unique()
-    event = pd.DataFrame(columns=['activity', 'continuity', 'coords'])
-
-    for coords in unique_coords:
-        current_events = df[df['coords'] == coords]
-        if len(current_events) > 1:
-            current_activity = len(current_events)
-            current_continuity = np.mean(np.diff(current_events['T'].to_numpy()))
-            event.loc[len(event)] = [current_activity, current_continuity, coords]
-    act_mean = event.activity.mean()
-    cont_mean = event.continuity.mean()
-    act_std = event.activity.std()
-    cont_std = event.continuity.std()
-    event = event[(event.activity > act_mean - 2*act_std) & (event.activity < act_mean + 2*act_std)]
-    event = event[(event.continuity > cont_mean - 3*cont_std) & (event.continuity < cont_mean + 3*cont_std)]
-    filtered_events = df[df['coords'].isin(event.coords.to_list())]
+    df = pd.DataFrame({'T': T, 'xEvent': xEvent, 'yEvent': yEvent})
+    df['coords'] = list(zip(df['xEvent'], df['yEvent']))
+    grouped = df.groupby('coords')
+    event = grouped.agg(activity=('coords', 'size'),
+                        continuity=('T', lambda x: np.mean(np.diff(sorted(x)))))
+    act_mean = event['activity'].mean()
+    act_std = event['activity'].std()
+    cont_mean = event['continuity'].mean()
+    cont_std = event['continuity'].std()
+    event_filtered = event[(event['activity'] > act_mean - 2 * act_std) & (event['activity'] < act_mean + 2 * act_std) &
+                           (event['continuity'] > cont_mean - 2 *cont_std) & (event['continuity'] < cont_mean + 2 * cont_std)]
+    filtered_events = df[df['coords'].isin(event_filtered.index)]
     return filtered_events['xEvent'].to_numpy(), filtered_events['T'].to_numpy(), filtered_events['yEvent'].to_numpy()
 
 
 def update(frame):
     startPoint = frame
-    endPoint = frame + 10000
+    endPoint = frame + event_num
     xEvent_selected = xEvent[startPoint:endPoint]
     T_selected = T[startPoint:endPoint]
     yEvent_selected = yEvent[startPoint:endPoint]
@@ -62,9 +49,23 @@ def init():
     return scat,
 
 
-xEvent_selected = xEvent[0:10000]
-T_selected = T[0:10000]
-yEvent_selected = yEvent[0:10000]
+with AedatFile(r'data/test_2mm_4Hz.aedat4') as f:
+    height, width = f['events'].size
+    events = np.hstack([packet for packet in f['events'].numpy()])
+    T, xEvent, yEvent, polarities = events['timestamp'], events['x'], events['y'], events['polarity']
+    T = (T - T[0])/1e6
+
+    xEvent = xEvent[0::10]
+    T = T[0::10]
+    yEvent = yEvent[0::10]
+    polarities = polarities[0::10]
+
+
+
+event_num = 30000
+xEvent_selected = xEvent[0:event_num]
+T_selected = T[0:event_num]
+yEvent_selected = yEvent[0:event_num]
 plt.rc('font', family='Times New Roman')
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
@@ -72,11 +73,11 @@ scat = ax.scatter(xEvent_selected, T_selected, yEvent_selected, marker='.')
 ax.set_xlabel('X', fontsize=18)
 ax.set_ylabel('T (s)', fontsize=18)
 ax.set_zlabel('Y', fontsize=18)
-plt.title('$Event from EventCamera of wave 2mm, 4Hz$')
+plt.title('Event from EventCamera of wave 2mm, 4Hz')
 ani = FuncAnimation(fig,
                     update,
-                    frames=np.arange(0, len(xEvent), 100),
-                    interval=100,
+                    frames=np.arange(0, len(xEvent), 300),
+                    interval=5,
                     init_func=init,
                     blit=False
                     )
